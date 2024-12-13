850357bd8dcfa96f050c62ef516950410846f9aa
RedFox197 <190109902+RedFox197@users.noreply.github.com>
2024-12-13 11:39:21 +0100

    Aggiustato

--- src/App.java
+++ src/App.java
@@ -10,44 +10,41 @@ public class App {
             for (int j = 0; j < griglia[i].length; j++) {
                 griglia[i][j] = '-';
             }
-        } 
-        
-        
-
-        for(int i=0; i<9; i++){
-            
-            int posizionaX, posizionaY;
-            boolean posizioneCorretta, continua=true;
-
-            do{
-                System.out.println("Posiziona la X");
-                posizionaX= sc.nextInt();
-                posizionaY = sc.nextInt();
-
-                posizioneCorretta=(posizionaX<=2 && posizionaX>=0) && (posizionaY<=2 && posizionaY>=0);
-
-                if(posizioneCorretta){
-                    if (griglia[posizionaY][posizionaX]=='-'){
-                        continua=false;
-                        griglia[posizionaY][posizionaX] = 'x';
-                        
-                    }
-                }else{
-                    System.out.println("la posizione è sbagliata");
-                }
-                
-    
-            }while(!posizioneCorretta||continua);
-            
-            for (int j = 0; j < griglia.length; j++) {
-                for (int k = 0; k < griglia[i].length; k++) {
-                    System.out.print(griglia[i][j]);
-                }
-                System.out.println();
-            } 
         }
 
-        
+
+        for (int i = 0; i < 9; i++) {
+            int posizionaX, posizionaY;
+
+            boolean posizioneCorretta, cellaOccupata;
+            do {
+                do {
+                    System.out.println("Posiziona la X");
+                    posizionaX = sc.nextInt();
+                    posizionaY = sc.nextInt();
+
+                    posizioneCorretta = (posizionaX <= 2 && posizionaX >= 0) && (posizionaY <= 2 && posizionaY >= 0);
+                    if (!posizioneCorretta) {
+                        System.out.println("la posizione è sbagliata");
+                    }
+                } while (!posizioneCorretta);
+
+                cellaOccupata = griglia[posizionaY][posizionaX] != '-';
+                if (cellaOccupata) {
+                    System.out.println("Questa cella è già occupata");
+                }
+            } while (cellaOccupata);
+
+            griglia[posizionaY][posizionaX] = 'X';
+
+            for (int j = 0; j < griglia.length; j++) {
+                for (int k = 0; k < griglia[j].length; k++) {
+                    System.out.print(griglia[j][k]);
+                }
+                System.out.println();
+            }
+        }
+
 
     }
 }
