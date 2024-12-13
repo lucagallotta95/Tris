929ffa49ec3fac234d78b3bc3311762f7a47d6a5
ClaudiaIuliano <claudia.iuliano2017@gmail.com>
2024-12-13 11:23:14 +0100

    da aggiustare

--- src/App.java
+++ src/App.java
@@ -12,18 +12,42 @@ public class App {
             }
         } 
         
-        System.out.println("Posiziona la X");
-        int posizionaX = sc.nextInt();
-        int posizionaY = sc.nextInt();
-        griglia[posizionaY][posizionaX] = 'x';
+        
 
+        for(int i=0; i<9; i++){
+            
+            int posizionaX, posizionaY;
+            boolean posizioneCorretta, continua=true;
 
-        for (int i = 0; i < griglia.length; i++) {
-            for (int j = 0; j < griglia[i].length; j++) {
-                System.out.print(griglia[i][j]);
-            }
-            System.out.println();
-        } 
+            do{
+                System.out.println("Posiziona la X");
+                posizionaX= sc.nextInt();
+                posizionaY = sc.nextInt();
+
+                posizioneCorretta=(posizionaX<=2 && posizionaX>=0) && (posizionaY<=2 && posizionaY>=0);
+
+                if(posizioneCorretta){
+                    if (griglia[posizionaY][posizionaX]=='-'){
+                        continua=false;
+                        griglia[posizionaY][posizionaX] = 'x';
+                        
+                    }
+                }else{
+                    System.out.println("la posizione è sbagliata");
+                }
+                
+    
+            }while(!posizioneCorretta||continua);
+            
+            for (int j = 0; j < griglia.length; j++) {
+                for (int k = 0; k < griglia[i].length; k++) {
+                    System.out.print(griglia[i][j]);
+                }
+                System.out.println();
+            } 
+        }
+
+        
 
     }
 }
