1e574400b8a8dca01ad90541e98782cbef3510ba
lucagallotta95 <lucagallotta95@gmail.com>
2024-12-13 10:12:18 +0100

    Passo1

--- src/App.java
+++ src/App.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner sc = new Scanner(System.in);
+        char[][] griglia = new char[3][3];
+
+        for (int i = 0; i < griglia.length; i++) {
+            for (int j = 0; j < griglia[i].length; j++) {
+                griglia[i][j] = '-';
+            }
+        } 
+        
+        System.out.println("Posiziona la X");
+        int posizionaX = sc.nextInt();
+        int posizionaY = sc.nextInt();
+        griglia[posizionaY][posizionaX] = 'x';
+
+
+        for (int i = 0; i < griglia.length; i++) {
+            for (int j = 0; j < griglia[i].length; j++) {
+                System.out.print(griglia[i][j]);
+            }
+            System.out.println();
+        } 
+
     }
 }
