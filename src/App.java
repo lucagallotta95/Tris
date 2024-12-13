d8a45d21c5fc599c00aa74801cdb6c59f44f6d03
lucagallotta95 <lucagallotta95@gmail.com>
2024-12-13 12:41:22 +0100

    Passo4

--- src/App.java
+++ src/App.java
@@ -1,9 +1,11 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         Scanner sc = new Scanner(System.in);
+        Random r = new Random();
         char[][] griglia = new char[3][3];
 
         for (int i = 0; i < griglia.length; i++) {
@@ -17,44 +19,49 @@ public class App {
             int posizionaX, posizionaY;
 
             boolean posizioneCorretta, cellaOccupata;
+            if (turnoUtente) {
+                System.out.println("\nPosiziona la X");
+            } else {
+                System.out.println("\nPosiziona la O");
+            } 
+            
             do {
                 do {
-                    if (turnoUtente){
-                        System.out.println("Posiziona la X");
-                    }
-                    else { 
-                        System.out.println("Posiziona la O");
-                    }
+
                     System.out.println();
-                    posizionaX = sc.nextInt();
-                    posizionaY = sc.nextInt();
+                    if (turnoUtente) {
+                        posizionaX = sc.nextInt();
+                        posizionaY = sc.nextInt();
+                    } else {
+                        posizionaX = r.nextInt(3);
+                        posizionaY = r.nextInt(3);
+                    }
 
                     posizioneCorretta = (posizionaX <= 2 && posizionaX >= 0) && (posizionaY <= 2 && posizionaY >= 0);
-                    if (!posizioneCorretta) {
+                    if (!posizioneCorretta && turnoUtente) {
                         System.out.println("la posizione è sbagliata");
                     }
                 } while (!posizioneCorretta);
 
                 cellaOccupata = griglia[posizionaY][posizionaX] != '-';
-                if (cellaOccupata) {
+                if (cellaOccupata && turnoUtente) {
                     System.out.println("Questa cella è già occupata");
                 }
             } while (cellaOccupata);
 
             if (turnoUtente) {
                 griglia[posizionaY][posizionaX] = 'X';
-            } 
-            else{
+            } else {
                 griglia[posizionaY][posizionaX] = 'O';
             }
-            turnoUtente=!turnoUtente;
-                
+            turnoUtente = !turnoUtente;
+
             for (int j = 0; j < griglia.length; j++) {
                 for (int k = 0; k < griglia[j].length; k++) {
-                        System.out.print(griglia[j][k]);
-                    }
-                    System.out.println();
+                    System.out.print(griglia[j][k]);
                 }
+                System.out.println();
+            }
         }
 
     }
